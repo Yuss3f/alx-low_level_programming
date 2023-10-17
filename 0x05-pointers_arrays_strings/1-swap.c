a8899e8a8d8f774b347c1984a781fd47d4708ae5
Youssef Kayk <kayke8youssef@gmail.com>
2023-10-17 02:38:27 -0700

    a

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int - swaps the values of two integers
- * 		using two input parameters
+ *		using two input parameters
  *
  * @a: input parameter 1
  * @b: input parameter 2
@@ -15,6 +15,6 @@ void swap_int(int *a, int *b)
 	int temp;
 
 	temp = *a;
-	*a =*b;
+	*a = *b;
 	*b = temp;
 }
