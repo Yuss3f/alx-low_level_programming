81b1a637e81a4bc4cfe8f3e50b55738cc750513a
Youssef Kayk <kayke8youssef@gmail.com>
2023-11-10 13:51:35 -0800

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
 	if (b < 0)
-		printf("Error\n"), exit (2);
-	
+		printf("Error\n"), exit(2);
+
 	while (b--)
 		printf("%02hhx%s", *p++, b ? " " : "\n";
 	return (0);
